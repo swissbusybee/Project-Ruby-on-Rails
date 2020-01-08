e4b93fe8b7eecaf502b4e205ebb07ce0b3218c5d
Sue Rocha <swissbusybee@gmail.com>
2020-01-08 22:41:31 +0100

    Tagging Feature completed

--- config/routes.rb
+++ config/routes.rb
@@ -5,3 +5,13 @@ Rails.application.routes.draw do
   resources :comments
   end
 end
+
+Blogger::Application.routes.draw do
+
+  root to: 'articles#index'
+  resources :articles do
+    resources :comments
+  end
+  resources :tags
+
+end
