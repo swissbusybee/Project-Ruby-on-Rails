97594f8b8ea1ba70c45750d2c3a8d91e8abee1ce
Sue Rocha <swissbusybee@gmail.com>
2020-01-09 15:41:46 +0100

    Sorcery authentication complete

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem "paperclip"
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'sorcery'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,15 @@ Rails.application.routes.draw do
 end
 
 Blogger::Application.routes.draw do
-
   root to: 'articles#index'
-  resources :articles do
-    resources :comments
-  end
-  resources :tags
 
+  resources :articles do
+  resources :comments
+  end
+
+  resources :tags
+  resources :authors
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+    get 'login'  => 'author_sessions#new'
+    get 'logout' => 'author_sessions#destroy'
 end
